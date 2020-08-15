110693db5a20ea005c2d3a35e02d2907f4772fdd
Denis Georgiev <dgeorgiev.web@gmail.com>
2020-08-15 18:16:19 +0300

    Defining Objects and Classes

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -1,22 +1,89 @@
 let gameComplete = false;
 // Define the three constants here
+const name = "unknown";
+const score = 0;
+const items = 0;
 
 // Define the player object here
+let player = {
+    name,
+    score,
+    items,
+
+    getCurrentScore() {
+        return this.score;
+    },
+
+    addPoints(points) {
+        score += points;
+    },
+    deductPoints(points) {
+        score -= points;
+    }
+}
 
 // Define the Product class - write the Constructor function for Product class here
+function Product(id, name, price, expiryDate) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.expiryDate = expiryDate;
+}
 
 // Complete the dateDiff function
-const dateDiff = (date1, date2) => {};
+const dateDiff = (date1, date2) => {
+    let timeDiff = Math.abs(date2.getTime() - date1.getTime()); //Use the Math object and common features
+
+    // days difference
+    let diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+    // difference
+    return diffDays;
+};
 
 // Here, use Object.defineProperty to create property - daysToExpire
+Object.defineProperties(Product.prototype, {
+    daysToExpire: {
+        get: function () {
+            return dateDiff(this.expiryDate, new Date());
+        },
+    }
+});
 
 // Add method getDetails to Product here
+Product.prototype.getDetails = function () {
+    return `Product Name: ${this.name} , Product Price: ${this.price}`
+}
 
 // Define the MagicProduct class here
-
+function MagicProduct(id, name, price, expirydate, points, isBonus) {
+    Product.call(this, id, name, price, expirydate);
+    this.points = points;
+    this.isBonus = isBonus;
+}
 // Establish inheritance between Product() & MagicProduct() here
+MagicProduct.prototype = Object.create(Product.prototype);
 
 // Define Rating class here
+class Rating {
+
+    //create the constructor
+    constructor() {
+        this.rate = "";
+    }
+
+    set rating(value) {
+       if(value > 1 && value <= 4) {
+            this.rate="OK";
+       } else if(value >= 5 && value <= 7) {
+            this.rate="GOOD";
+       } else if(value > 7) {
+            this.rate="EXCEPTIONAL";
+       } else {
+            this.rate="BAD";
+       }
+    }
+}
 
 // Complete the loadProducts function
 const loadProducts = (map, prodId) => {
